6a6298a1f904931cfa179da3aa2cc03d8e9b2b84
abuliegeguolage <123>
2023-07-05 17:22:22 +0800

    不完全的精靈圖改造

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,26 @@
+<script setup>
+
+    const headerSpirits = ['卡片總覽', '分類排行', '通知'];
+
+</script>
+
 <template>
     <div class="header">
-        <a href="/" class="logo">這裡之後放logo</a>
+        <a href="/" class="logo">CCC - Credit Card Comparison</a>
         <div class="search">
             <input type="search" name="" id="">
             <button type="button"></button>
         </div>
         <ul>
-            <li><a href="#" class="allCardsIcon">試算優惠</a></li>
-            <li><a href="#" class="rankingsIcon">分類排行</a></li>
-            <li><a href="#" class="notificationIcon">卡片總覽</a></li>
-            <li><a href="#" class="profilePic"></a></li>
+            <li v-for="(name, index) in headerSpirits">
+                <a href="#" class="aIcon" :style="{ backgroundPositionX: 11.3-index*41.8 + 'px' }">
+                    {{ name }}
+                </a>
+            </li>
+            <li>
+                <a href="#" class="profilePic"></a>
+                
+            </li>
         </ul>
     </div>
 </template>
@@ -18,29 +29,30 @@
 
     .header {
         overflow: hidden;
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
         position: fixed;
         z-index: 2;
+        width: 100vw;
         height: 80px;
         background-color: #81D9EC;
 
         .logo {
-            position: absolute;
-            margin-left: 40px;
-            width: 160px;
+            width: 400px;
             height: 100%;
             background-image: '';
             /* 下面這段有logo之後刪掉 */
-            color: gray;
-            background-color: lightpink;
+            color: #009DBF;
+            font-size: 30px;
             text-align: center;
             text-decoration: none;
             line-height: 80px;
         }
 
         .search {
-            position: absolute;
+            position: relative;
             overflow: hidden;
-            margin: 20px 0 20px 420px;
             width: 400px;
             height: 40px;
             background-color: white;
@@ -71,47 +83,47 @@
         ul {
             display: flex;
             align-items: center;
-            justify-content: space-around;
-            margin: 10px 40px 10px 950px;
-            width: 280px;
+            justify-content: space-evenly;
+            width: 330px;
 
-            a {
-                display: inline-block;
-                width: 60px;
-                height: 60px;
-                text-decoration: none;
-                text-align: center;
-                line-height: 8;
-                font-size: 12px;
-                color: black;
-                background: no-repeat center 10px;
-                background-size: 24px 24px;
+            li {
+
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                
+                .aIcon {
+                    display: inline-block;
+                    width: 50px;
+                    height: 50px;
+                    text-decoration: none;
+                    text-align: center;
+                    line-height: 7.6;
+                    font-size: 12px;
+                    color: white;
+                    background: url('../assets/images/headerSpirits.svg') no-repeat left 0;
+                    background-size: 300%;
+                    transition: .2s;
+                    &:hover {
+                        color: #009DBF;
+                        background-image: url('../assets/images/headerSpiritsHover.svg');
+                    }
+                }
+
+                .profilePic {
+                    width: 50px;
+                    height: 50px;
+                    border: 1px solid gray;
+                    border-radius: 50%;
+                    background: white url('../assets/images/預設頭像.svg') no-repeat center 8px;
+                    background-size: 60%;
+                    &:hover {
+                        
+                    }
+                }
+
             }
 
-        }
-
-        .allCardsIcon {
-            background-image: url('../assets/images/卡片總覽.svg');
-            color: white;
-        }
-
-        .rankingsIcon {
-            background-image: url('../assets/images/分類榜.svg');
-            color: white;
-        }
-
-        .notificationIcon {
-            background-image: url('../assets/images/小鈴鐺.svg');
-            color: white;
-        }
-
-        .profilePic {
-            width: 40px;
-            height: 40px;
-            border: 1px solid gray;
-            border-radius: 50%;
-            background: white url('../assets/images/預設頭像.svg') no-repeat center 8px;
-            background-size: 60%;
         }
 
     }
