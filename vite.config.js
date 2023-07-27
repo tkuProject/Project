47c05f054ce304f55a7cbc610b073ad7eee5550a
abuliegeguolage <123>
2023-07-27 23:34:08 +0800

    在vite.config.js加了一些設定

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs'
+  },
   base: '/Project/',
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0'
+  }
 })
