60585d19ce376aaab53f71987cc6ebccfc86fa18
janet <123>
2023-08-10 22:28:21 +0800

    插入平台名稱和照片

---
+++ insert_into.sql
@@ -0,0 +1,8 @@
+--shopping_Platform
+
+INSERT INTO Shopping_Platform (sName, sImg_Site) values ('蝦皮', 'https://assets.meet.jobs/uploads/organization/1028/logo/profile_270px-Shopee_Taiwan_logo.svg__1_.png?1645673158' );
+INSERT INTO Shopping_Platform (sName, sImg_Site) values ('Momo', 'https://deo.shopeemobile.com/shopee/shopee-mobilemall-live-sg/assets/55ac4ccbc1752c1423341d8ef48d4489.png');
+INSERT INTO Shopping_Platform (sName, sImg_Site) values ('PChome 24小時', 'https://fs.ecimg.tw/css/2016/style/images/v201607/sitelogo/h24.png');
+INSERT INTO Shopping_Platform (sName, sImg_Site) values ('PChome商店街', 'https://image.cache.storm.mg/styles/smg-800x533-fp/s3/media/image/2018/07/02/20180702-104859_U12167_M427878_a1f9.PNG?itok=_Q6XWbrU');
+INSERT INTO Shopping_Platform (sName, sImg_Site) values ('Yahoo奇摩超級商城', 'https://img.scupio.com/gym/image/logo/yahoo-mall.png');
+INSERT INTO Shopping_Platform (sName, sImg_Site) values ('Yahoo購物中心', 'https://img.scupio.com/gym/image/logo/yahoo.png');
