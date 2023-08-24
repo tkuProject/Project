93976f17e9e57e083382722b7a29e91025b0cfe9
janet <123>
2023-08-24 15:18:13 +0800

    修改全部購物平台圖片網址

--- insert_into.sql
+++ insert_into.sql
@@ -1,8 +1,8 @@
 --shopping_Platform
 
-INSERT INTO Shopping_Platform (sName, sImg_Site) values ('蝦皮', 'https://assets.meet.jobs/uploads/organization/1028/logo/profile_270px-Shopee_Taiwan_logo.svg__1_.png?1645673158' );
-INSERT INTO Shopping_Platform (sName, sImg_Site) values ('Momo', 'https://deo.shopeemobile.com/shopee/shopee-mobilemall-live-sg/assets/55ac4ccbc1752c1423341d8ef48d4489.png');
-INSERT INTO Shopping_Platform (sName, sImg_Site) values ('PChome 24小時', 'https://fs.ecimg.tw/css/2016/style/images/v201607/sitelogo/h24.png');
-INSERT INTO Shopping_Platform (sName, sImg_Site) values ('PChome商店街', 'https://image.cache.storm.mg/styles/smg-800x533-fp/s3/media/image/2018/07/02/20180702-104859_U12167_M427878_a1f9.PNG?itok=_Q6XWbrU');
-INSERT INTO Shopping_Platform (sName, sImg_Site) values ('Yahoo奇摩超級商城', 'https://img.scupio.com/gym/image/logo/yahoo-mall.png');
-INSERT INTO Shopping_Platform (sName, sImg_Site) values ('Yahoo購物中心', 'https://img.scupio.com/gym/image/logo/yahoo.png');
+INSERT INTO Shopping_Platform (sName, sImg_Site) values ('蝦皮', 'https://cdngarenanow-a.akamaihd.net/shopee/shopee-pcmall-live-tw/assets/6b05141182eecfa218574f88e1e62cd4.jpg' );
+INSERT INTO Shopping_Platform (sName, sImg_Site) values ('Momo', 'https://fashionstw.com/wp-content/uploads/2020/05/momo%E8%B3%BC%E7%89%A9-LOGO.jpg');
+INSERT INTO Shopping_Platform (sName, sImg_Site) values ('PChome 24小時', 'https://2pir-sport.com/wp-content/uploads/2018/08/pchomelogo-730x350.jpg');
+INSERT INTO Shopping_Platform (sName, sImg_Site) values ('PChome商店街', 'https://www.enermax.com/archive/location/item/PCH_Logo_532px.jpg');
+INSERT INTO Shopping_Platform (sName, sImg_Site) values ('Yahoo奇摩超級商城', 'https://s.yimg.com/ma/auc/common/logo/og-mall-19.png');
+INSERT INTO Shopping_Platform (sName, sImg_Site) values ('Yahoo購物中心', 'https://invoice.tw/news/wp-content/uploads/2018/01/yahoobuy.png');
