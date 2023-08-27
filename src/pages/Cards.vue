06f7491a9c7ddd452f55dc973574fd6c230ecafe
abuliegeguolage <123>
2023-08-27 22:17:19 +0800

    卡片總覽的搜尋關鍵字完成

--- src/pages/Cards.vue
+++ src/pages/Cards.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-    import { computed, reactive } from 'vue';
+    import { computed, reactive, ref } from 'vue';
     import { useCardStore } from '../store/CardStore';
     import { useUserStore } from '../store/userStore';
     import sendReq from '../utils/sendReq';
@@ -105,29 +105,55 @@
     };
     */
 
+    const searchingMode = ref(false);
+    const keyIn = ref('');
+    let keywordToShow;
+    const keyNos = reactive([]);
+    const search = () => {
+        sendReq('searchCards', {
+            query: {
+                keyIn: keyIn.value
+            }
+        }).then(json => {
+            if(json.status == 200) {
+                keyNos.length = 0;
+                keyNos.push(...json.cardNos);
+                keywordToShow = keyIn.value;
+                searchingMode.value = true;
+            }
+        });
+    }
+
 </script>
 
 <template>
     
     <div class="search">
-        <input type="search" placeholder="請輸入感興趣的卡片關鍵字">
-        <button type="button"></button>
+        <input v-model="keyIn" type="search" placeholder="請輸入感興趣的卡片關鍵字">
+        <button @click="search" class="searchBtn" type="button"></button>
+    </div>
+    <button @click="searchingMode = false" v-if="searchingMode" class="goBack">返回</button>
+
+    <div v-if="searchingMode" class="searchHint">
+        與 {{ keywordToShow }} 相關的卡片：
     </div>
 
     <template v-if="userStore.account">
         <span>我收藏的卡片：</span>
         <ul v-if="collectionCards?.length >= 0">
-            <li v-for="card in collectionCards">
-                <CardPreview :card="card">
-                    <template #header v-if="userStore.account">
-                        <!-- <button v-if="notiOnCards.includes(card.Card_No)" @click="turnOffNoti(card.Card_No)" class="informBtn" title="關閉通知" :style="{ backgroundImage: `url('` + (infoOffImg) + `')` }">
-                        </button>
-                        <button v-else @click="turnOnNoti(card.Card_No)" class="informBtn" title="開啟通知" :style="{ backgroundImage: `url('` + (infoOnImg) + `')` }">
-                        </button> -->
-                        <button @click="delFromCollection(card.Card_No)" class="addAndDel delFromCollection" title="從您的收藏中刪除">×</button>
-                    </template>
-                </CardPreview>
-            </li>
+            <template v-for="card in collectionCards">
+                <li v-if="searchingMode ? keyNos.includes(card?.Card_No) : true">
+                    <CardPreview :card="card">
+                        <template #header v-if="userStore.account">
+                            <!-- <button v-if="notiOnCards.includes(card.Card_No)" @click="turnOffNoti(card.Card_No)" class="informBtn" title="關閉通知" :style="{ backgroundImage: `url('` + (infoOffImg) + `')` }">
+                            </button>
+                            <button v-else @click="turnOnNoti(card.Card_No)" class="informBtn" title="開啟通知" :style="{ backgroundImage: `url('` + (infoOnImg) + `')` }">
+                            </button> -->
+                            <button @click="delFromCollection(card.Card_No)" class="addAndDel delFromCollection" title="從您的收藏中刪除">×</button>
+                        </template>
+                    </CardPreview>
+                </li>
+            </template>
             <div v-if="collectionCards.length==0" class="noCard">目前沒有收藏卡片喔～</div>
         </ul>
         <div v-else>讀取中</div>
@@ -136,17 +162,19 @@
 
     <span>{{ userStore.account?'其他卡片：':'卡片列表' }}</span>
     <ul v-if="(userStore.account?otherCards:cardStore.allCards)?.length >= 0">
-        <li v-for="card in userStore.account?otherCards:cardStore.allCards">
-            <CardPreview :card="card">
-                <template #header v-if="userStore.account">
-                    <!-- <button v-if="notiOnCards?.includes(card.Card_No)" @click="turnOffNoti(card.Card_No)" class="informBtn" title="關閉通知" :style="{ backgroundImage: `url('` + (infoOffImg) + `')` }">
-                    </button>
-                    <button v-else @click="turnOnNoti(card.Card_No)" class="informBtn" title="開啟通知" :style="{ backgroundImage: `url('` + (infoOnImg) + `')` }">
-                    </button> -->
-                    <button @click="addToCollection(card.Card_No)" class="addAndDel addToCollection" title="加入您的收藏">+</button>
-                </template>
-            </CardPreview>
-        </li>
+        <template v-for="card in userStore.account?otherCards:cardStore.allCards">
+            <li v-if="searchingMode ? keyNos.includes(card?.Card_No) : true">
+                <CardPreview :card="card">
+                    <template #header v-if="userStore.account">
+                        <!-- <button v-if="notiOnCards?.includes(card.Card_No)" @click="turnOffNoti(card.Card_No)" class="informBtn" title="關閉通知" :style="{ backgroundImage: `url('` + (infoOffImg) + `')` }">
+                        </button>
+                        <button v-else @click="turnOnNoti(card.Card_No)" class="informBtn" title="開啟通知" :style="{ backgroundImage: `url('` + (infoOnImg) + `')` }">
+                        </button> -->
+                        <button @click="addToCollection(card.Card_No)" class="addAndDel addToCollection" title="加入您的收藏">+</button>
+                    </template>
+                </CardPreview>
+            </li>
+        </template>
         <div v-if="(userStore.account?otherCards:cardStore.allCards).length==0" class="noCard">目前沒有其他卡片喔～</div>
     </ul>
     <div v-else>讀取中</div>
@@ -175,13 +203,34 @@
         }
         button {
             position: absolute;
+            border: none;
             right: 0;
             width: 10%;
             height: 100%;
             background: url('../assets/images/放大鏡.svg') no-repeat center;
             background-size: 40%;
-            border: none;
         }
+    }
+    .goBack {
+        position: absolute;
+        top: 80px;
+        width: 80px;
+        height: 40px;
+        text-align: right;
+        font-size: 20px;
+        background: url(../assets/images/goBack.svg) no-repeat center left;
+        background-size: 30%;
+        border: none;
+        &:hover {
+            color: #009DBF;
+            background-image: url(../assets/images/goBackHover.svg);
+        }
+    }
+
+    .searchHint {
+        margin: 50px 0;
+        text-align: center;
+        font-size: 20px;
     }
 
     hr {
