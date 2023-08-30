9a67c09b68e8e39b3d893652bae306382e943a00
abuliegeguolage <123>
2023-08-30 19:43:31 +0800

    確認使用者有登入才發請求拿收藏卡片

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,15 @@
   const userStore = useUserStore();
   
   onBeforeMount(async () => {
+    // 請求所有卡片
+    await cardStore.getCards();
     // 確認是否已有登入
     const account = localStorage.getItem('account');
     if(account) {
       userStore.account = account;
+      // 把收藏拿過來
+      await userStore.getCollectionCards();
     }
-    // 請求所有卡片
-    await cardStore.getCards();
-    // 把收藏拿過來
-    await userStore.getCollectionCards();
   });
 
 </script>
