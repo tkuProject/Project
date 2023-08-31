6c5fc38eca9348cec4927604179a645f19b58e67
janet <123>
2023-09-01 00:11:46 +0800

    蝦皮的拿掉了

--- insert_into.sql
+++ insert_into.sql
@@ -1,6 +1,6 @@
 --shopping_Platform
 
-INSERT INTO Shopping_Platform (sName, sImg_Site) values ('蝦皮', 'https://cdngarenanow-a.akamaihd.net/shopee/shopee-pcmall-live-tw/assets/6b05141182eecfa218574f88e1e62cd4.jpg' );
+
 INSERT INTO Shopping_Platform (sName, sImg_Site) values ('Momo', 'https://fashionstw.com/wp-content/uploads/2020/05/momo%E8%B3%BC%E7%89%A9-LOGO.jpg');
 INSERT INTO Shopping_Platform (sName, sImg_Site) values ('PChome 24小時', 'https://2pir-sport.com/wp-content/uploads/2018/08/pchomelogo-730x350.jpg');
 INSERT INTO Shopping_Platform (sName, sImg_Site) values ('PChome商店街', 'https://www.enermax.com/archive/location/item/PCH_Logo_532px.jpg');
